84ae4f5a0da581c2cff16e25cd634a73d1f91223
Sebastian Ramírez <sebasramirez.dev@gmail.com>
2021-03-22 22:01:20 -0500

    use props in badge component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
+
+  
   render(){
+    //PROPS: Argumentos a una funcion. en este caso atributos a nuestro componente
+    const {firstname, lastname, avatar, jobtitle, twitter} = this.props;
+    
     return(
-      <div>
-        <div>
+      <div className="badge">
+        <div className="badge_header">
           <img src={ confLogo } alt="Logo conferencia"/>
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
-          <h1>Sebastian <br/> Ramirez</h1>
+        <div className="badge_section-name">
+          <img className="badge_avatar" src={avatar} alt="Avatar"/>
+          <h1>{firstname} <br/> {lastname}</h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@elpuutas</p>
+        <div className="badge_section-info">
+          <h3>{jobtitle}</h3>
+          <div>@{twitter}</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="badge_footer">#platziconf</div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import './global.css';
 import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge/>, container); //render(que, dónde)
+ReactDOM.render(
+  <Badge
+    firstname="Sebastián"
+    lastname="Ramírez" 
+    avatar="https://www.gravatar.com/avatar?d=identicon"
+    jobtitle="Frontend Engineer" 
+    twitter="elpuutas"
+  />, 
+  container); //render(que, dónde)
 
